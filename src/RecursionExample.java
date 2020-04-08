ece4b2d5aea726cff9ca74a0706a38588292cd0b
Tadej Slemenšek <tadejslemensek9@gmail.com>
2020-04-08 19:41:26 +0200

    added comment

--- src/RecursionExample.java
+++ src/RecursionExample.java
@@ -1,6 +1,9 @@
 public class RecursionExample {
 
     public static void main(String[] args) {
+
+        //Don't go over 60ish. For the sake of your computer :D
+        //I'm advising yout, to debug the process and take a look at the variables over time.
         System.out.println(fibonacciCalc(2));
     }
 
